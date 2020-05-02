6c7d1bee2c08925faffcf54b7b28001465b47402
Rebecca <becca.siegal@gmail.com>
2020-05-02 10:52:04 -0400

    Incorporated Al's edits

--- test/app.spec.js
+++ test/app.spec.js
@@ -10,17 +10,39 @@ describe("App", () => {
 });
 
 
-const getAllBlogpostss = require('../blogposts/blogposts-service');
+// you should put the blogpost tests in their own file
+
+// Declare the database connection so that you can inject it into any function that depends on it.
+const knex = require('knex');
+let db;
+
+
+// the service file exports the entire service object, so that's what you need to require here. Then, you'll use this const to get access to the specific function, like 'blogpostsService.getAllBlogposts()'
+
+const blogpostsService = require('../src/blogposts/blogposts-service');
 
 describe('getAllBlogposts', () => {
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,  // don't know if this works with heroku
+    })
+    app.set('db', db)
+  })
+
+  after('disconnect from db', () => db.destroy())
   it('should return a list of all the blogs in the database', () => {
     // define inputs
+        // what is the intention for the following three lines? Is it really a complicated 'const' declaration, or should it be three separate statements. This looks like it was copied from somewhere. What is 'i' doing? It wasn't declared originally. What is 'updatedArray' doing? It doesn't seem to be used. Finally, why are you indexing into blogsArray for your expected answer when you later expect to get the whole array?
+        const i=0; // temp to get tests to run
+
+
     const blogsArray = [],
       updatedArray = blogsArray + i,
       expectedAnswer = blogsArray[i];
 
     // invoke the function
-    const actualAnswer = getAllBlogpostss(blogsArray);
+    const actualAnswer = blogpostsService.getAllBlogposts(db, blogsArray);
 
     // assert that expected === actual
     expect(actualAnswer).to.equal(expectedAnswer);
@@ -28,12 +50,14 @@ describe('getAllBlogposts', () => {
 
   it('should throw an error when there are no blogs in the database', () => {
     // define inputs
-    const blogsArray, i;
+      // const blogsArray, i;
+      const blogsArray =[];
 
     // set up the function call
-    const fn = () => { getAllBlogpostss(blogsArray) };
+    const fn = () => { blogpostsService.getAllBlogposts(db, blogsArray) };
 
     // assert that exception is thrown
+        // NOTE: there is no code in the getAllBlogposts function to throw an exception. Is this test supposed to pass?
     expect(fn).to.throw();
   });
 });
